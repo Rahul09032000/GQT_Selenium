efc97252a17d744544ea04c2ae2b425f5934f308
Rahul <rahulmahendrakar09@gmail.com>
2022-08-03 22:13:30 +0530

    Codes Updated on 3rd August 2022

---
+++ GQT_Selenium/src/com/gqt/selenium/Launch27.java
@@ -0,0 +1,22 @@
+// HTML code of Parent Page 
+// <a href="Page2.html" target="blank"> Go to child Page </a>
+package com.gqt.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch27 {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("F:/GQT Selenium/HTML for Selenium/page1.html");
+		
+		WebElement link = driver.findElement(By.linkText("Go to child Page"));
+		link.click();
+		Thread.sleep(5000);
+		
+		driver.close();
+	}
+}
